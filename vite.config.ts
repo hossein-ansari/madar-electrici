0dc27c367b585273f6c0bc50474c7cd46465b0bf
Hossein-ansari <hossein838485@gmail.com>
2025-05-20 21:51:44 -0700

    fix bug deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-    open: true
-  },
+  base: '/madar-electrici/',
+
   build: {
     outDir: 'dist',
-    sourcemap: true
   }
 })
